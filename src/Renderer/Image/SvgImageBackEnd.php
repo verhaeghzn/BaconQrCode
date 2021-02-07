ff0815ff20204b6ea54ee66a4f3b6a0122bcafbc
Michel Verhaegh <48225570+MV-ICT@users.noreply.github.com>
2021-02-07 14:57:24 +0100

    Update SvgImageBackEnd.php
    sprintf('scale(%s)', round($size, self::PRECISION)) aangepast naar
    sprintf('scale(%s)', round($size, 0))

--- src/Renderer/Image/SvgImageBackEnd.php
+++ src/Renderer/Image/SvgImageBackEnd.php
@@ -97,7 +97,7 @@ final class SvgImageBackEnd implements ImageBackEndInterface
         $this->xmlWriter->startElement('g');
         $this->xmlWriter->writeAttribute(
             'transform',
-            sprintf('scale(%s)', round($size, self::PRECISION))
+            sprintf('scale(%s)', round($size, 0))
         );
         ++$this->stack[$this->currentStack];
     }
@@ -111,7 +111,7 @@ final class SvgImageBackEnd implements ImageBackEndInterface
         $this->xmlWriter->startElement('g');
         $this->xmlWriter->writeAttribute(
             'transform',
-            sprintf('translate(%s)', round($x, self::PRECISION))
+            sprintf('translate(%s,%s)', round($x, self::PRECISION), round($y, self::PRECISION))
         );
         ++$this->stack[$this->currentStack];
     }
